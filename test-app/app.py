9e6207cdc51a3ae8c213c9e0fe02b67540401ae8
sandromayer <41449594+sandromayer@users.noreply.github.com>
2024-09-03 22:46:10 +0200

    Create app.py

---
+++ test-app/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Set the title of the web app
+st.title("Hello, World App")
+
+# Display "Hello, World!" message
+st.write("Hello, World!")
+
+# Optional: You can add more elements like sliders, buttons, etc.
+# st.button("Click Me")
