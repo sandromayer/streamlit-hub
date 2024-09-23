0de0bb80008ca173ee8e11498be990aa71d8bba9
sandromayer <41449594+sandromayer@users.noreply.github.com>
2024-09-24 00:10:50 +0200

    Update main.py

--- test_app3/main.py
+++ test_app3/main.py
@@ -10,7 +10,7 @@ st.title("OIDC Authentifizierte Streamlit App")
 # Beispiel einer Funktion mit zwei Gruppen von Rollen:
 # - Gruppe 1: Der Benutzer muss "a" ODER "b" haben.
 # - Gruppe 2: Der Benutzer muss "c" ODER "d" haben.
-@auth.require_any_role(["test_role"])
+@auth.require_any_role(["test_role2"])
 @auth.require_any_role(["test_role"])
 def protected_section():
     st.write("Sie haben Zugriff, weil Sie mindestens eine Rolle aus jeder Gruppe besitzen.")
